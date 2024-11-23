59f2a840d2f1ffc24be0e15395b45343c0ef12e7
vinarahmavionita <vinarahmavionita824@gmail.com>
2024-11-23 23:11:19 +0700

    menambahkan inputan bibit

---
+++ router/bibit.js
@@ -0,0 +1,25 @@
+const express = require('express');
+
+const router = express.Router();
+
+let bibit = [
+    {
+        id: "1",
+        Nama: "semangka",
+        stok: "100",
+        Harga: 30000,
+    },
+    {
+        id: "2",
+        Nama: "kiwi",
+        stok: "90",
+        Harga: 70000,
+    },
+    {
+        id: "3",
+        Nama: "anggur",
+        stok: "150",
+        Harga: 90000,
+    },
+];
+
