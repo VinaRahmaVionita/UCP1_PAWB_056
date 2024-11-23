7a97f4eb6c47bd9f569c9f4ab8f50c99978dccdb
vinarahmavionita <vinarahmavionita824@gmail.com>
2024-11-23 23:15:09 +0700

    menambahkan data pupuk

---
+++ router/pupuk.js
@@ -0,0 +1,25 @@
+const express = require('express');
+
+const router = express.Router();
+
+const pupuk = [
+    {
+        id: "01",
+        Nama: "pupuk urea",
+        stok: "100",
+        Harga: 12000,
+    },
+    {
+        id: "02",
+        Nama: "pupuk kompos",
+        stok: "150",
+        Harga: 20000,
+    },
+    {
+        id: "03",
+        Nama: "pupuk npk",
+        stok: "200",
+        Harga: 30000,
+    },
+];
+
